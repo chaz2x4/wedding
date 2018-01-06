b70b64a33aa1c0a2f888c4fba61657278d7b4d79
Chaz George <chaz2x4@gmail.com>
2018-01-05 19:15:17 -0600

    Upload engagement photos and thumbnails

---
+++ reduceImages.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+var { createCanvas, Image } = require('canvas');
+
+function resizeBase64Img() {
+  var image_dir = 'images/engagement';
+  fs.readdir(image_dir, (err, files) => {
+    if (err) return console.error(err);
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i];
+      if(path.extname(file) === '.jpg') {
+        fs.readFile(path.join(image_dir, file), (err, data) => {
+          if (err) return console.error(err);
+
+          var img = new Image;
+          img.src = data;
+          var w = img.width/10;
+          var h = img.height/10;
+          var canvas = createCanvas(w, h);
+          var context = canvas.getContext('2d');
+
+          context.drawImage(img, 0, 0, w, h);
+
+          var out = fs.createWriteStream(path.join('images/thumbnails', i+'.jpg'));
+          var stream = canvas.createJPEGStream({ bufsize: 4096, quality: 75 });
+
+          stream.pipe(out);
+        });
+      }
+    }
+  });
+}
+resizeBase64Img();
